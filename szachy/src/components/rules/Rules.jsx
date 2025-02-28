ae6591cdf75416bebf8b0b2c01ac901421b35dbd
emiliawslk <emiliawslk@gmail.com>
2025-02-28 15:00:58 +0100

    css merged YIPPIE

--- szachy/src/components/rules/Rules.jsx
+++ szachy/src/components/rules/Rules.jsx
@@ -4,7 +4,8 @@ function Rules() {
     return (
             <>
                 <div id="wrapper4">
-                    <p>
+                <div id="container">
+
                         
 
                         <div id="king"><img src="./obrazki/krolek.webp" alt="krol"/><br />Król może ruszać się o jedno pole w poziomie, pionie lub na ukos. Najwyżej raz w grze każdy z króli może wykonać specjalny ruch zwany roszadą.</div>
@@ -13,7 +14,8 @@ function Rules() {
                         <div id="queen"><img src="./obrazki/hetman.webp" alt="Hetman"/><br /> Hetman porusza się o dowolną liczbę wolnych pól w poziomie, pionie i na ukos.</div>
                         <div id="horse"><img src="./obrazki/kon.webp" alt="kon"/><br />Ruchy skoczka opisuje się także porównując je do litery „L” lub cyfry „7” (lub dowolnej odbitej, w poziomie i pionie, ich postaci), przy czym wykonuje on dwa kroki w danym kierunku, 90° zwrot w jednym z kierunków i krok w nowo wybranych kierunku. Ruch skoczka nie jest blokowany przez inne bierki, tzn. „skacze” on na nowe pole. , </div>
                         <div id="pawn"><img src="./obrazki/pionek.webp" alt="pion"/><br /> W pierwszym ruchu ma on możliwość wykonania ruchu o dwa pola naprzód, o ile żadne z tych pól nie jest zajęte. Przez resztę gry porusza się o jedno pole naprzód. Pion nie może ruszać się do tyłu. Piony są jedynymi bierkami, które biją inaczej niż się poruszają. Mogą one zbić wrogą bierkę, jeśli znajduje się ona na jednym z dwóch pól sąsiadujących w poziomie z polem przed nimi. Piony mają również dwa szczególne posunięcia: en passant oraz promocję .</div>
-                    </p>
+
+                </div>
                 </div>
             </>
         )
